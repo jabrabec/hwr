189bc9ddd5a2c3c290c8b58b6a45f76e324aa171
Jennifer Brabec <jenniferbrabec@gmail.com>
2017-06-12 18:49:24 -0700

    ch. 13 completed

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import Chance from 'chance';
 
 class Detail extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const people = [];
+
+        for (let i = 0; i < 10; i++) {
+            people.push({
+                name: chance.first(),
+                country: chance.country({ full: true })
+            });
+        }
+
+        this.state = { people };
+    }
+
+    buttonClicked() {
+        const newState = {
+            name: chance.first()
+        };
+
+        this.setState(newState);
+    }
+
     render() {
-        return <p>This is React rendering HTML!</p>;
+        return (<div>
+        {this.state.people.map((person, index) => (
+            <p key={index}>Hello, {person.name} from {person.country}!</p>
+        ))}
+        </div>);
     }
 }
 
